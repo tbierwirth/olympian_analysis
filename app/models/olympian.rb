f267267c8c67d2fafba83b4aaa93379b49eb9392
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-11-03 11:50:07 -0700

    Setup has_many associations through olympian_event

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,5 +1,7 @@
 class Olympian < ApplicationRecord
   belongs_to :team
+  has_many :olympian_events
+  has_many :events, through: :olympian_events
 
   validates_presence_of :name, :sex, :age, :height, :weight
   validates_presence_of :team_id
