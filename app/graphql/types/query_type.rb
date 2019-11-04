c413ea21b28f642c38fa7eaaa55f0fa173f9d3fe
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-11-03 20:52:53 -0700

    Add query to return the youngest olympian

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,8 +3,16 @@ module Types
     field :olympians, [Types::OlympianType], null: false do
       description "Return all olympians"
     end
+
     def olympians
       Olympian.all
+    end
+
+    field :youngest_olympian, Types::OlympianType, null: false do
+      description "Return an olympian"
+    end
+    def youngest_olympian
+      Olympian.order(:age).first
     end
   end
 end
